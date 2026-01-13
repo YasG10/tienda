ca59e91bab1aa2c3b751a066b8e440afa84e41c4
YaStick <yasseryasg@gmail.com>
2026-01-13 16:01:39 -0500

    bd

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -94,8 +94,18 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_PZu3ITFAS6kR',
+        'HOST': 'ep-falling-sound-ah4oj3nd-pooler.c-3.us-east-1.aws.neon.tech',
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+            'channel_binding': 'require'
+        },
+
+
     }
 }
 
